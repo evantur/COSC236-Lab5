cd3f88d533e4a5d31af7c4868e63f5b52fb29716
Evan <eboyct@gmail.com>
2024-11-19 19:20:11 -0800

    Update EBook.java

--- src/lab5/EBook.java
+++ src/lab5/EBook.java
@@ -5,28 +5,29 @@ public class EBook implements Book{
 	private String title;
 	private Boolean isAvailable;
 
+	public EBook(String title) {
+		this.title = title;
+		this.isAvailable = true;
+	}
 	@Override
 	public String getTitle() {
-		// TODO Auto-generated method stub
-		return null;
+		return title;
 	}
-
 	@Override
 	public void setTitle(String title) {
-		// TODO Auto-generated method stub
 		this.title = title;
 	}
-
+	@Override
+	public String toString() {
+		return "EBook: " + title;
+	}
 	@Override
 	public void setIsAvailable(boolean isAvailable) {
-		// TODO Auto-generated method stub
 		this.isAvailable = isAvailable;
 	}
-
 	@Override
 	public boolean getIsAvailable() {
-		// TODO Auto-generated method stub
-		return false;
+		return isAvailable;
 	}
 
 }
