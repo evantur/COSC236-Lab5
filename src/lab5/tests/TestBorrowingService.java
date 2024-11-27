9ff0f8d9f851b5bf1ba81d4f24598e55d126fcf4
Evan <eboyct@gmail.com>
2024-11-26 19:01:43 -0800

    Update TestBorrowingService.java

--- src/lab5/tests/TestBorrowingService.java
+++ src/lab5/tests/TestBorrowingService.java
@@ -1,13 +1,24 @@
 package lab5.tests;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+
+import lab5.Book;
+import lab5.BorrowingService;
+import lab5.Member;
+import lab5.PaperBook;
 
 class TestBorrowingService {
 
+	BorrowingService service = new BorrowingService();
+	Member george = new Member("George");
+	Book book1 = new PaperBook("The Two Towers");
+	
 	@Test
 	void BorrowingService() {
+		// test creation
+		
+		
+		// test return
 		
 	}
 
