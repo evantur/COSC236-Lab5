d3025496029e7db317cff1a8e7f096b5d7c1c195
Evan <eboyct@gmail.com>
2024-11-26 18:57:57 -0800

    added testfile

---
+++ src/lab5/tests/TestBorrowingService.java
@@ -0,0 +1,14 @@
+package lab5.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestBorrowingService {
+
+	@Test
+	void BorrowingService() {
+		
+	}
+
+}
